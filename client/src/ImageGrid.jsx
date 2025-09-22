076250324408c5550114ed1f5a945996db0b41d4
gogarx <zandogo@gmail.com>
2025-09-22 12:39:30 -0300

    adicionado masonry ao layout

--- client/src/ImageGrid.jsx
+++ client/src/ImageGrid.jsx
@@ -5,28 +5,26 @@ import { setCurrentImage } from './data/actions.js';
 import { imageGroups, sortFunction } from './sorting';
 import { slugify } from './utils';
 import { withImages } from './wrappers';
+import Masonry from 'react-masonry-css';
+
 
 export const ImageCard = ({ image, imageSize, onClick }) => (
-    <div className="ui card" onClick={onClick}>
-        <div className="image">
-            <LazyLoad key={image.event_id}>
-                <img
-                    className={'ui image ' + imageSize}
-                    src={
-                        imageSize === 'massive'
-                            ? image.image_url
-                            : image.thumbnail_url
-                    }
-                />
-            </LazyLoad>
-        </div>
-        <div className="content">
-            <div className="meta">
-                Posted {image.timestamp.format('h:mm A, ddd MMM Do')}
-            </div>
-            <div className="meta">By {image.sender}</div>
-        </div>
+  <div className="ui card" onClick={onClick} style={{ width: '100%' }}>
+    <div className="image" style={{ width: '100%' }}>
+      <LazyLoad key={image.event_id}>
+        <img
+          className={'ui image ' + imageSize}
+          style={{ width: '100%', height: 'auto', display: 'block' }}
+          src={
+            imageSize === 'massive'
+              ? image.image_url
+              : image.thumbnail_url
+          }
+          alt={image.alt || 'image'}
+        />
+      </LazyLoad>
     </div>
+  </div>
 );
 
 const ImageGridComponent = ({
@@ -58,18 +56,21 @@ const ImageGridComponent = ({
                     id={slugify(title || 0)}
                 >
                     {title && <h2>{title}</h2>}
-                    <div className="ui six doubling cards">
-                        {_.sortBy(images, sortFunction(sortOrder)).map(
-                            image => (
-                                <ImageCard
-                                    key={image.event_id}
-                                    image={image}
-                                    imageSize={imageSize}
-                                    onClick={() => setCurrentImage(image)}
-                                />
-                            )
-                        )}
-                    </div>
+                    <Masonry
+                        breakpointCols={{ default: 6, 1100: 4, 700: 3, 500: 2 }}
+                        className="my-masonry-grid"
+                        columnClassName="my-masonry-grid_column"
+                    >
+                        {_.sortBy(images, sortFunction(sortOrder)).map(image => (
+                            <ImageCard
+                                key={image.event_id}
+                                image={image}
+                                imageSize={imageSize}
+                                onClick={() => setCurrentImage(image)}
+                            />
+                        ))}
+                    </Masonry>
+
                 </div>
             ))}
         </div>
